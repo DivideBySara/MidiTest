fb41c80f8e8edeb3917363c6086ae87f2b31f85d
Sara Jade <wisconsinsara@gmail.com>
2017-10-26 22:45:39 -0700

    Update credits

--- MidiTest/Program.cs
+++ MidiTest/Program.cs
@@ -9,6 +9,10 @@ namespace MidiTest
     {        
         static void Main(string[] args)
         {
+            /// <summary>
+            /// Midi sequence source(s): http://www.kuhmann.com/Yamaha.htm, Tchaikovsky, Pyotr I., last updated 11-May-2016, downloaded 10/25/17
+            /// Author: author / sequencer / performer and / or Robert C. Kuhmann 
+            /// </summary>
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "002TCHAIKOVSKYPyotrIlyichRachmaninovSleepingWaltzseqYogore.mid");
             string openCommand = "open " + filePath + " alias music";
 
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # MidiTest
 Proof of concept that I can play midis in console apps with C# and Visual Studio.
 
-Special thanks to [Peter Shaw](http://www.codeguru.com/columns/dotnet/making-music-with-midi-and-c.html) for documenting midi requirements.
+Special thanks to 
+1) [Peter Shaw](http://www.codeguru.com/columns/dotnet/making-music-with-midi-and-c.html) for documenting midi requirements.
+2) Midi sequence source(s): http://www.kuhmann.com/Yamaha.htm, Tchaikovsky, Pyotr I., last updated 11-May-2016, downloaded 10/25/17
+Author: author/sequencer/performer and/or Robert C. Kuhmann
